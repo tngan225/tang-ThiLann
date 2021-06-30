0457254aef1c2a73b82973fcdedc2b87b12ff3f9
tngan225 <86741384+tngan225@users.noreply.github.com>
2021-07-01 01:35:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Crush àaaaa',
+    introDesc: ` Việt Nam có hơn 90 triệu người mà em lại biết được chị thì có gọi là may mắn quá khonggg nhờ!!!
+    từ thích thích
+    thành iu iu 
+    rùi thương thươngg,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnYes: 'thích tiet ngan nhieuuuu lam',
+    btnNo: 'Khônggg, chị không thích em đouu :)',
+    question:'Tại sao chị lại thích emmm',
+    btnReply: 'Gửi cho tiet ngann',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    mess: 'em biết màaaa, Yêu chị nhiều nhiều ',
+    messDesc: 'ráng đợi em lên đại học nhoooo',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
